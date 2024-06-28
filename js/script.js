7a3dbacd3a60e5f7c286de7d0cfccede5e31f92d
eclivse <ecaoliviaaa@gmail.com>
2024-06-29 03:00:54 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// window.onload = () => {
+//     const userName = prompt("Hi, please enter your name! ^^");
+//     const userNameSection = document.getElementById("username");
+//     if(userName != null){
+//         userNameSection.innerHTML += userName;
+//     } else {
+//         userNameSection.innerHTML += "there"
+//     }
+// }
